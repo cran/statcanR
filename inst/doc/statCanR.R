8ce8d7cfc9309b04adcdb617ce77a67ed23d2f91
Thierry Warin <thierry.warin@hec.ca>
2023-03-03 14:30:02 +0000

    version 0.2.4

--- inst/doc/statCanR.R
+++ inst/doc/statCanR.R
@@ -14,7 +14,10 @@ library(statcanR)
 library(statcanR)
 
 ## ---- eval=FALSE--------------------------------------------------------------
+#  # Identify with statcan_search() function
+#  statcan_search(c("federal","expenditures"),"eng")
+
+## ---- eval=FALSE--------------------------------------------------------------
 #  # Get data with statcan_data function
-#  mydata <- statcan_data("27-10-0014-01", "eng")
-#  
+#  mydata <- statcan_download_data("27-10-0014-01", "eng")
 
